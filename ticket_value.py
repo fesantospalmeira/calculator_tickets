df0aff92ad55328ff74f76fe9ca1a1f542a8bc7e
fesantospalmeira <felipesantos7938@gmail.com>
2025-05-06 17:37:54 -0300

    Teste Commit

--- ticket_value.py
+++ ticket_value.py
@@ -27,4 +27,5 @@ def total_value(ages, count_tickets):
             raise ValueError('Crianças com menos de 1 ano não pagam ingresso.')
         else:
             raise ValueError('A idade não pode ser negativa')
+    print('Valor total calculado com sucesso!')
     return total
